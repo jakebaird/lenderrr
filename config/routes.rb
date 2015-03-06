530f4852313f4af6560b70df00c7fd676bed88c1
hg_140 <hendrik.gunawan@codecampz.com>
2015-03-06 07:36:44 +0700

    fix scope devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
+  devise_for :users, skip: [:sessions, :registrations]
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   
@@ -8,14 +8,14 @@ Rails.application.routes.draw do
 
   #->Prelang (user_login:devise/stylized_paths)
   devise_scope :user do
-    get    "login"   => "users/sessions#new",         as: :new_user_session
-    post   "login"   => "users/sessions#create",      as: :user_session
-    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
+    get    "login"   => "devise/sessions#new",         as: :new_user_session
+    post   "login"   => "devise/sessions#create",      as: :user_session
+    delete "signout" => "devise/sessions#destroy",     as: :destroy_user_session
 
-    get    "signup"  => "users/registrations#new",    as: :new_user_registration
-    post   "signup"  => "users/registrations#create", as: :user_registration
-    put    "signup"  => "users/registrations#update", as: :update_user_registration
-    get    "account" => "users/registrations#edit",   as: :edit_user_registration
+    get    "signup"  => "devise/registrations#new",    as: :new_user_registration
+    post   "signup"  => "devise/registrations#create", as: :user_registration
+    put    "signup"  => "devise/registrations#update", as: :update_user_registration
+    get    "account" => "devise/registrations#edit",   as: :edit_user_registration
   end
 
   resources :items
