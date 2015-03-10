607fe4526d55742a9c2f3213d599fafe0836ea2f
Hendra Gunawan <hendragunz@codecampz.com>
2015-03-10 10:46:05 +0700

    fix sign in + sign out (layout)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,36 +3,35 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
-
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
-    #->Prelang (user_login:devise)
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-      devise_parameter_sanitizer.for(:sign_in)        { |u| u.permit(:login, :email, :password, :remember_me) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-    end
 
-  private
-    #-> Prelang (user_login:devise)
-    def require_user_signed_in
-      unless user_signed_in?
+  #->Prelang (user_login:devise)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in)        { |u| u.permit(:login, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
 
-        # If the user came from a page, we can send them back.  Otherwise, send
-        # them to the root path.
-        if request.env['HTTP_REFERER']
-          fallback_redirect = :back
-        elsif defined?(root_path)
-          fallback_redirect = root_path
-        else
-          fallback_redirect = "/"
-        end
+  # #-> Prelang (user_login:devise)
+  # def require_user_signed_in
+  #   unless user_signed_in?
 
-        redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
-      end
-    end
+  #     # If the user came from a page, we can send them back.  Otherwise, send
+  #     # them to the root path.
+  #     if request.env['HTTP_REFERER']
+  #       fallback_redirect = :back
+  #     elsif defined?(root_path)
+  #       fallback_redirect = root_path
+  #     else
+  #       fallback_redirect = "/"
+  #     end
 
-  
+  #     redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
+  #   end
+  # end
+
+
 
 end
