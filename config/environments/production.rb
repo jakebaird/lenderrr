7eb1164c66b5001d1cca2e8ecbb23e88181d5ebd
hg_140 <hendrik.gunawan@codecampz.com>
2015-03-13 14:54:32 +0700

    devise gmail confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options    = { :host => 'lenderrr.herokuapp.com' }
-  config.action_mailer.delivery_method        = :smtp
+  config.action_mailer.default_url_options    = { :host => 'lenderrr.herokuapp.com', :protocol => 'http' }
   config.action_mailer.perform_deliveries     = true
-  config.action_mailer.raise_delivery_errors  = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings          = {
     :port                  =>  587,
